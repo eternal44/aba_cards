3460e66fd0ed8aa8b920aedf5f2bcdd4ab24784f
James Youn <jamesyoun710@gmail.com>
2018-06-11 13:16:54 -0700

    Adds drawer scaffold

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react"
-import { StyleSheet, View, Text, PanResponder, Animated,  } from "react-native"
+import SideMenu from 'react-native-side-menu';
+import {
+  StyleSheet,
+  View,
+  Text,
+  PanResponder,
+  Animated,
+  TouchableOpacity,
+  Image
+} from "react-native"
+
 import Draggable from './components/Draggable'
 import Draggables from './components/Draggables'
+import Menu from './components/Menu';
 
 const DRAGGABLES = [
   '1',
@@ -12,26 +23,8 @@ const DRAGGABLES = [
   '6'
 ]
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.mainContainer}>
-        <Draggables
-          ref={draggable => this._draggablesComponent = draggable }
-          draggables={DRAGGABLES}
-        />
-        <View style={styles.dropZone}>
-          <Text style={styles.text}>Drop here to remove!</Text>
-        </View>
-      </View>
-    )
-  }
-}
-
+const image = require('./assets/menu.png');
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1
-  },
   dropZone: {
     backgroundColor: "#00334d",
     marginTop: "auto"
@@ -44,5 +37,76 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 25,
     fontWeight: "bold"
+  },
+  button: {
+    position: 'absolute',
+    top: 20,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
   }
-})
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  onMenuItemSelected = item =>
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+
+  render() {
+    const menu = <Menu />;
+
+    return (
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+      >
+        <View style={styles.container}>
+          <Draggables
+            ref={draggable => this._draggablesComponent = draggable }
+            draggables={DRAGGABLES}
+          />
+          <View style={styles.dropZone}>
+            <Text style={styles.text}>Drop here to remove!</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={this.toggle}
+          style={styles.button}
+        >
+          <Image
+            source={image}
+            style={{ width: 32, height: 32 }}
+          />
+        </TouchableOpacity>
+      </SideMenu>
+    )
+  }
+}
